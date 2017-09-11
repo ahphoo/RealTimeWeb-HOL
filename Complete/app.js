8186447c6ac2c9a4879a2fdb776fa89fe4e653e2
AP PH <ahphu@ucsd.edu>
2017-09-10 23:13:38 -0700

    got past all dependecies errors and now stuck on VM invalid opcode

--- Complete/app.js
+++ Complete/app.js
@@ -1,28 +1,3 @@
-/*// Import the page's CSS. Webpack will know what to do with it.
-import "/stylesheets/app.css";
-
-// Import libraries we need.
-import { default as Web3} from 'web3';
-import { default as contract } from 'truffle-contract'
-
-/*
- * When you compile and deploy your Game contract,
- * truffle stores the abi and deployed address in a json
- * file in the build directory. We will use this information
- * to setup a Game abstraction. We will use this abstraction
- * later to create an instance of the Game contract.
- * Compare this against the index.js from our previous tutorial to see the difference
- * https://gist.github.com/maheshmurthy/f6e96d6b3fff4cd4fa7f892de8a1a1b4#file-index-js
- *
-
-import decentralized_artifacts from '/build/contracts/decentralized.json'
-
-
-var decentralized = contract(decentralized_artifacts);
-
-window. */
-
-
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
@@ -32,7 +7,6 @@ var io = require('socket.io')(http);
 
 /* Set up web3 library */
 var Web3 = require('web3');
-/*
 if (typeof web !== 'undefined') {
 	web3 = new Web3(web3.currentProvider);
 } else {
@@ -40,35 +14,34 @@ if (typeof web !== 'undefined') {
 	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 var fs = require('fs');
-var code = fs.readFileSync('./contracts/Bank.sol').toString();
-//var code = fs.readFileSync('./contracts/decentralized.sol').toString();
+var code = fs.readFileSync('./contracts/ChainChess.sol').toString();
 var solc = require('solc');
 var compiledCode = solc.compile(code);
 
-//Deploy the contract 
-abiDefinition = JSON.parse(compiledCode.contracts[':Bank'].interface);
-//abiDefinition = JSON.parse(compiledCode.contracts[':decentralized'].interface);
+/*Deploy the contract */
+abiDefinition = JSON.parse(compiledCode.contracts[':ChainChess'].interface);
 GameContract = web3.eth.contract(abiDefinition);
-byteCode = compiledCode.contracts[':Bank'].bytecode;
-//byteCode = compiledCode.contracts[':decentralized'].bytecode;
-deployedContract = GameContract.new( {data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
+byteCode = compiledCode.contracts[':ChainChess'].bytecode;
+deployedContract = GameContract.new( web3.eth.accounts[1], {data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
+deployedContract.address; //I don't know, maybe wait a cycle for contract to deploy?
 contractInstance = GameContract.at(deployedContract.address);
+
 web3.eth.defaultAccount=web3.eth.accounts[0]; //set default account 
+web3.eth.sendTransaction( {from: web3.eth.accounts[0], to: deployedContract.address, value:1000000000000000000, gas: 290000} ); //send 1 ether
 
-
-
-//contractInstance.forward(web3.eth.defaultAccount); */
+/*
 var web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider("http://localhost:8545"));
-abi = JSON.parse('[{"constant":false,"inputs":[],"name":"payBlack","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"payWhite","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getContractCreationValue","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"player_two","type":"address"}],"payable":true,"stateMutability":"payable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]');
+abi = JSON.parse('[{"constant":false,"inputs":[],"name":"payBlack","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"getPot","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"payWhite","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[{"name":"player2","type":"address"}],"payable":true,"stateMutability":"payable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]');
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0x41db9dff8a029e426cd879f7bafcfad4ae559b3a');
+contractInstance = VotingContract.at('0x41db9dff8a029e426cd879f7bafcfad4ae559b3a'); */
 
-
+/* Handle HTML request and provide path to default.html */
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/default.html');
 });
 
+/* Display port number */
 http.listen(port, function() {
     console.log('listening on *: ' + port);
 });
@@ -81,12 +54,12 @@ io.on('connection', function(socket) {
     });
 
     socket.on('status', function(msg) {
+
     	socket.broadcast.emit('status',msg);
 	console.log(msg);
 	if(msg === 'Game over, White is in checkmate.') {
 		//Pay player 1
 		contractInstance.payBlack();
-		
 	}
 	else if(msg === 'Game over, Black is in checkmate.') {
 		//Pay player 2
--- Complete/contracts/ChainChess.sol
+++ Complete/contracts/ChainChess.sol
@@ -1,55 +1,54 @@
-﻿pragma solidity ^0.4.16;
+pragma solidity ^0.4.16;
 
 contract ChainChess {
 
-    address white;
-    address black;
-    uint pot; // original endowment
-    
-    /* Fall-back function */
-    function() payable
-    {
-        pot += msg.value;
-    }
+	address white;
+	address black;
+	uint pot; // original endowment
 
-    function ChainChess(address player2) public payable
-    {
-        white = msg.sender; 		
-        black = player2;
-        pot = msg.value;  				// the starting pot for chess game
-    }
-    
-    function payWhite() payable
-    {
-        white.transfer(pot);
-        pot -= pot;
-    }
-    
-    function payBlack() payable
-    {
-        black.transfer(pot);
-        pot -= pot;
-    }
-	
-    function getPot() constant returns (uint) // returns the total ether held by the contract
-    {										              		
-    	return pot;  
-    }
-        
-    /**********
-     Return ether to both players
-     **********
-    
-    function kill()
-    { 
-        if (msg.sender == white)
-        {
-            suicide(white);  // kills this contract and sends remaining funds back to creator
-        }
-        if (msg.sender == black)
-        {
-            suicide(black);
-        }
-    }
-    */
+	/* Fall-back function */
+	function() payable
+	{
+		pot += msg.value;
+	}
+
+	function ChainChess(address player2) public payable
+	{
+		white = msg.sender; 		
+		black = player2;
+		pot = msg.value;  	// the starting pot for chess game
+	}
+
+	function payWhite() payable
+	{
+		white.transfer(pot);
+		pot -= pot;
+	}
+
+	function payBlack() payable
+	{
+		black.transfer(pot);
+		pot -= pot;
+	}
+
+	function getPot() constant returns (uint) // returns the total ether held by the contract
+	{										              		
+		return pot;  
+	}
+
+	/**********
+	  Return ether to both players
+	 **********/
+
+	function kill() payable
+	{ 
+		if (msg.sender == white)
+		{
+			suicide(white);  // kills this contract and sends remaining funds back to creator
+		}
+		if (msg.sender == black)
+		{
+			suicide(black);
+		}
+	}
 }
